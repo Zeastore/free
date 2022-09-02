f179ae139bffdf2bb5967e65e7215b55ace33866
Zea King Store <98459024+Zeastore@users.noreply.github.com>
2022-09-02 21:09:35 +0700

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -3,25 +3,6 @@ dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Dat
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
-red='\e[1;31m'
-green='\e[1;32m'
-NC='\e[0m'
-clear
-echo "Add Domain"
-echo "Progress..."
-sleep 2
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-PERMISSION
-if [ -f /home/needupdate ]; then
-red "Your script need to update first !"
-exit 0
-elif [ "$res" = "Permission Accepted..." ]; then
-echo -ne
-else
-red "Permission Denied!"
-exit 0
-fi
 
 sub=$(</dev/urandom tr -dc a-z | head -c4)
 DOMAIN=bhm-vpn.com
